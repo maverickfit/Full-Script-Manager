d64aa3c1e885e0f0c1eac06b157176a0824ca725
maverickfit <mavericks@ifit.com>
2020-10-01 15:52:57 -0600

    Updates ip window to work in class creation

--- File_Manager/IP.py
+++ File_Manager/IP.py
@@ -4,16 +4,16 @@ import os
 
 class IP:
 	def __init__(self):
-		ip_window = Tk()
-		ip_window.title('IP Address')
-		self.address = ttk.Entry(ip_window, width = 12)
-		self.button = ttk.Button(ip_window, text = 'Click me', command = self.clicked)
+		self.ip_window = Tk()
+		self.ip_window.title('IP Address')
+		self.address = ttk.Entry(self.ip_window, width = 12)
+		self.button = ttk.Button(self.ip_window, text = 'Click me', command = self.clicked)
 		self.address.pack()
 		self.button.pack()
-		ip_window.mainloop()
+		self.ip_window.mainloop()
 	
 	def clicked(self):
-		print(self.address.get())
+		self.ip_window.destroy()
 	
 	def Get_IP(self):
-		return self.address
+		return self.address.get()
