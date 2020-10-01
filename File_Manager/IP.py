d1057ca1484a21030d3e27a419f6992b12c38e77
maverickfit <mavericks@ifit.com>
2020-10-01 15:05:02 -0600

    Moves IP window to IP.py and adds get for ip

--- File_Manager/IP.py
+++ File_Manager/IP.py
@@ -3,4 +3,17 @@ from tkinter import ttk
 import os
 
 class IP:
-	address = ttk.Entry()
+	def __init__(self):
+		ip_window = Tk()
+		ip_window.title('IP Address')
+		self.address = ttk.Entry(ip_window, width = 12)
+		self.button = ttk.Button(ip_window, text = 'Click me', command = self.clicked)
+		self.address.pack()
+		self.button.pack()
+		ip_window.mainloop()
+	
+	def clicked(self):
+		print(self.address.get())
+	
+	def Get_IP(self):
+		return self.address
--- full_script_manager.py
+++ full_script_manager.py
@@ -7,12 +7,9 @@ def clicked():
 	print(IP.address.get())
 
 def main():
-	root = Tk()
-	IP.address = ttk.Entry(root, width = 12)
-	button_script1 = ttk.Button(root, text = 'Click me', command = clicked)
-	IP.address.pack()
-	button_script1.pack()
-	root.mainloop()
+	start = IP()
+	
+	
 	
 	
 if __name__ == '__main__': main()
