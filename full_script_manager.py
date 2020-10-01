3f7d4fafeea869d3bead2c1f7dfab97b697df783
maverickfit <mavericks@ifit.com>
2020-10-01 13:24:15 -0600

    Adds ip entry box

--- full_script_manager.py
+++ full_script_manager.py
@@ -3,13 +3,14 @@ from tkinter import ttk
 import os
 
 def clicked():
-	print(click)
+	print(tablet_IP.get())
 
 def main():
 	root = Tk()
-	global click
-	click = 'You got me'
+	global tablet_IP
+	tablet_IP = ttk.Entry(root, width = 12)
 	button_script1 = ttk.Button(root, text = 'Click me', command = clicked)
+	tablet_IP.pack()
 	button_script1.pack()
 	root.mainloop()
 	
