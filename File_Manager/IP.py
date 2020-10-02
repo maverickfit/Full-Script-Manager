5c0b916c01f370599483e293c4b62f026ecb943a
maverickfit <mavericks@ifit.com>
2020-10-02 10:56:03 -0600

    Adds label for entry and corrects window format

--- File_Manager/IP.py
+++ File_Manager/IP.py
@@ -8,12 +8,21 @@ class IP:
 	def __init__(self):
 		self.ip_window = Tk()
 		self.ip_window.title('IP Address')
+		
+		self.footer = ttk.Frame(self.ip_window, width = 200, height = 30)
+		
+		self.label = ttk.Label(self.ip_window, text = 'Please enter target IP address')
 		self.address = ttk.Entry(self.ip_window, width = 12)
-		self.start_button = ttk.Button(self.ip_window, text = 'Start', command = self.Start)
-		self.cancel_button = ttk.Button(self.ip_window, text = 'Cancel', command = self.Cancel)
-		self.address.pack()
-		self.start_button.pack()
-		self.cancel_button.pack()
+		self.start_button = ttk.Button(self.footer, text = 'Start', command = self.Start)
+		self.cancel_button = ttk.Button(self.footer, text = 'Cancel', command = self.Cancel)
+		
+		self.label.pack()
+		self.address.pack(fill = X)
+		self.footer.pack(fill = X)
+		
+		self.start_button.grid(row = 0, column = 0)
+		self.cancel_button.grid(row = 0, column = 1)
+		
 		self.ip_window.mainloop()
 	
 	def Start(self):
