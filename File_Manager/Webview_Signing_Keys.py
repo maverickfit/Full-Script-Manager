2d85d3efce60fb3992b38a2ea633638e099a511d
maverickfit <mavericks@ifit.com>
2020-10-02 14:59:40 -0600

    Removes un-needed template items and starts webview class

--- File_Manager/Webview_Signing_Keys.py
+++ File_Manager/Webview_Signing_Keys.py
@@ -2,16 +2,6 @@ from tkinter import *
 from tkinter import ttk
 import os
 
-def clicked():
-	print(click)
-
-def main():
-	root = Tk()
-	global click
-	click = 'You got me'
-	button_script1 = ttk.Button(root, text = 'Click me', command = clicked)
-	button_script1.pack()
-	root.mainloop()
-	
-	
-if __name__ == '__main__': main()
+class Webview:
+	Webview_Path = ''
+	Package_Name = ''
