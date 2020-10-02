c9c148123d133b61b007d14e431ee871bb0660a4
maverickfit <mavericks@ifit.com>
2020-10-02 11:35:28 -0600

    Adds scirpt frame and corrects header/footer span
    * Adds script frame and grids it
    * Adds welcome and ip labels to header
    * Changes header/footer rowspan to columnspan
    * Adds notes for each frame and their griding

--- full_script_manager.py
+++ full_script_manager.py
@@ -14,12 +14,26 @@ def main():
 		main_window = Tk()
 		main_window.title('Full Script Manager')
 		
-		header = ttk.Frame(main_window, width = 300, height = 20)
-		header.grid(row = 0, column = 0, rowspan = 4, sticky = EW)
+		#Header frame and widgets
+		header = ttk.Frame(main_window, width = 400, height = 20)
+		welcome = ttk.Label(header, text = 'Welcome to the Full Script Manager')
+		ip_display = ttk.Label(header, text = 'IP: ' + ip_address.Get_IP())
+		#Header griding
+		header.grid(row = 0, column = 0, columnspan = 4, sticky = EW)
+		welcome.grid(row = 0, column = 0)
+		ip_display.grid(row = 0, column = 20)
 		
-		footer = ttk.Frame(main_window, width = 300, height = 20)
+		#Script frame and widgets
+		script_frame = ttk.Frame(main_window, width = 200, height = 200)
+		#Script griding
+		script_frame.grid(row = 1, column = 1, sticky = EW)
+		
+		
+		#Footer frame and widgets
+		footer = ttk.Frame(main_window, width = 400, height = 20)
 		exit_button = ttk.Button(footer, text = 'Quit', command = close_script)
-		footer.grid(row = 20, column = 0, rowspan = 4, sticky = EW)
+		#Footer griding
+		footer.grid(row = 20, column = 0, columnspan = 4, sticky = EW)
 		exit_button.grid(row = 0, column = 20)
 		
 		main_window.mainloop()
