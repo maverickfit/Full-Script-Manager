2b93b964ba41171c61774113e1aa4be63e22eb02
maverickfit <mavericks@ifit.com>
2020-10-06 13:23:44 -0600

    Adds main window with title

--- File_Manager/Installer.py
+++ File_Manager/Installer.py
@@ -4,3 +4,12 @@ import os
 
 class Installer:
 	_IP = ''
+	
+	def __init__(self, IP_Address):
+		#Building main window
+		global installer_window
+		installer_window = Tk()
+		installer_window.title('Installer')
+		
+		#Main loop
+		installer_window.mainloop()
