f5f1e1f2c322ee712a3da83e3ab1ca6d4bea508c
maverickfit <mavericks@ifit.com>
2020-10-07 15:57:03 -0600

    Builds installer window
    * Adds ip address required property
    * Adds new top level window for installer
    * Names new window installer
    * Withdrawns master window

--- File_Manager/Installer.py
+++ File_Manager/Installer.py
@@ -3,8 +3,14 @@ from tkinter import ttk
 
 class Installer:
 
-    def __init__(self, master):    
-        pass
+    def __init__(self, master, ip_address):    
+        self._IP = ip_address
+        
+        self.installer_window = TopLevel(master)
+        self.installer_window.title('Installer')
+        master.state('withdrawn')
+        
+        self.installer_window.mainloop()
 
             
 def main():            
