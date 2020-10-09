8700b55f71fa27478ac43163a1a8600f1b56c03b
maverickfit <mavericks@ifit.com>
2020-10-09 13:05:58 -0600

    Adds idle collector to project

---
+++ File_Manager/Idle_Collector.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+import logging
+import os
+from tkinter import messagebox
+
+class Collector:
+
+    def __init__(self, master): 
+        logging.info('Idle Collector was started')   
+
+            
+def main():            
+    
+    root = Tk()
+    Collector(root)
+    root.mainloop()
+    
+if __name__ == "__main__": main()
