ef40b3c38f07322f7632dec006af9d8fea6ab24e
maverickfit <mavericks@ifit.com>
2020-10-09 13:24:29 -0600

    Adds main window and header/footer construction
    * Adds internal ip variable to sent ipaddress

--- File_Manager/Idle_Collector.py
+++ File_Manager/Idle_Collector.py
@@ -8,14 +8,30 @@ class Collector:
     _IP = ''
     _workout_time = 0
 
-    def __init__(self, master):
+    def __init__(self, master, ip_address):
         logging.info('Idle Collector was started')
+
+        self._IP = ip_address
+
+        #builds main idle collector window
+        self.idle_window = Toplevel(master)
+        self.idle_window.title('Idle Collector')
+        master.state(['widthdrawn'])
+
+        #frame builder
+        self.header_frame = ttk.Frame(self.idle_window, width = 300, height = 50)
+        self.header_frame.pack(expand = True, fill = HORIZONTAL)
+
+        self.footer_frame = ttk.Frame(self.idle_window, width = 300, height = 50)
+        self.footer_frame.pack(side = BOTTOM, expand = True, fill = HORIZONTAL)
+
+        self.idle_window.mainloop()
 
             
 def main():            
     
     root = Tk()
-    Collector(root)
+    Collector(root, 'ipaddress')
     root.mainloop()
     
 if __name__ == "__main__": main()
