13467305f0e4ad7f95fa00a801ae5b0325ffbfc5
maverickfit <mavericks@ifit.com>
2020-10-12 14:21:55 -0600

    Adds shell automation file to project

---
+++ File_Manager/Shell_Automation.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+
+class Automation:
+
+    def __init__(self, master):
+        pass
+
+            
+def main():            
+    
+    root = Tk()
+    Automation(root)
+    root.mainloop()
+    
+if __name__ == "__main__": main()
