3ed2885080e40d98ab851be7b02fda150a369047
maverickfit <mavericks@ifit.com>
2020-10-12 15:49:06 -0600

    Adds HTML5 test and changes all .sh to say test

--- File_Manager/Shell_Automation.py
+++ File_Manager/Shell_Automation.py
@@ -17,7 +17,7 @@ class Automation:
                     if messagebox.askyesno(title = 'Connection method', message = 'Are you connected to the tablet via a wired USB OTG cable (not WiFi)?'):
                         os.chdir('File_Manager/Shell_Scripts')
                         logging.info('Testing started')
-                        webview_key = subprocess.Popen(['./webview-signing-key-check.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+                        webview_key = subprocess.Popen(['./webview-signing-key-test.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
                         stdout, stderr = webview_key.communicate()
                         if stderr != '':
                             messagebox.showerror(title = 'Webview Signing Key', message='Unable to complete Webview Signing Key Check due to an error: {}'.format(stderr))
@@ -35,7 +35,7 @@ class Automation:
                                     messagebox.showerror(title = 'Geekbench Compute Test', message = 'Unable to complete Geekbench Compute test due to an error: {}'.format(stderr))
                                 else:
                                     logging.info('Geekbench compute test ran: {}'.format(stdout))
-                                    memory = subprocess.Popen(['./memory-verification.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+                                    memory = subprocess.Popen(['./memory-verification-test.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
                                     stdout, stderr = memory.communicate()
                                     if stderr != '':
                                         messagebox.showerror(title = 'Memory Verification', message='Unable to complete Memory Verification due to an error: {}'.format(stderr))
@@ -47,13 +47,18 @@ class Automation:
                                             messagebox.showerror(title = 'Processor Bit Size', message='Unable to complete Processor Bit Size due to an error: {}'.format(stderr))
                                         else:
                                             logging.info('Processor Bit Size test ran: {}'.format(stdout))
-                                            webview_version = subprocess.Popen(['./webview-version-check.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+                                            webview_version = subprocess.Popen(['./webview-version-test.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
                                             stdout, stderr = webview_version.communicate()
                                             if stderr != '':
                                                 messagebox.showerror(title='Webview Version', message='Unable to complete Webview Version Check due to an error: {}'.format(stderr))
                                             else:
                                                 logging.info('Webview Version test ran: {}'.format(stdout))
-                                                
+                                                html5 = subprocess.Popen(['.html5-test.sh'], text = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+                                                stdout, stderr = html5.communicate()
+                                                if stderr != '':
+                                                    messagebox.showerror(title='HTML5 Test', message='Unable to complete HTML5 test due to an error: {}'.format(stderr))
+                                                else:
+                                                    logging.info('HTML5 test ran: {}'.format(stdout))
                     else:
                         messagebox.showerror(title = 'Connection method', message = 'Connect to the testing tablet with a wired USB OTG connection and run the script again.')
                         master.state(['normal'])
